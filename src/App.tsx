eb8c42946b68e00df5af6acd2f063167ead13189
Breno Lemos <brenomiros@gmail.com>
2021-06-02 15:03:49 -0300

    Creates GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <h1>Pokedex</h1>
     </div>
   );
