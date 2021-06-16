b9fc4074c14dba9482ca91abab26ab5194568679
Breno Lemos <brenomiros@gmail.com>
2021-06-16 17:24:12 -0300

    New tests for Badge component

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Badge from './Badge';
+import typeColors from '../../helpers/typeColors';
 
 it('should render the text passed as prop', () => {
   render(<Badge type="fire" />);
 
   const badge = screen.getByText('Fire');
   expect(badge).toBeInTheDocument();
+});
+
+it('should render the correct color', () => {
+  render(<Badge type="fire" />);
+
+  const badge = screen.getByText('Fire');
+  expect(badge).toHaveStyle({ backgroundColor: typeColors.fire });
+});
+
+it('should render the correct color when the type is not in typeColors object', () => {
+  render(<Badge type="bazinga" />);
+
+  const badge = screen.getByText('Bazinga');
+  expect(badge).toHaveStyle({ backgroundColor: '#5d5e60' });
 });
